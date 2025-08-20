7b1eacb60b8a420c34a8b98801de53eac8c578b9
Emil Iapparov <emealeaque@gmail.com>
2025-08-20 17:11:23 +0500

    Fix 1.14

--- L1.14/main.go
+++ L1.14/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "reflect")
 
 func WhatType(v interface{}){ //interface{} == any
     switch v.(type){
@@ -10,10 +12,13 @@ func WhatType(v interface{}){ //interface{} == any
             fmt.Println("string")
         case bool:
             fmt.Println("bool")
-        case chan int:
-            fmt.Println("chan")
-        default:
-            fmt.Println("Неизвестная переменная")
+        default:    
+            rv := reflect.ValueOf(v)// проверяем через reflect, если это канал любого типа
+            if rv.Kind() == reflect.Chan {
+                fmt.Println("chan")
+            } else {
+                fmt.Println("Неизвестная переменная")
+            }
     }
 }
 
@@ -22,10 +27,20 @@ func main() {
     v2 := "WB"
     v3 := true
     v4 := make(chan int)
+    v5 := make(chan string)
 
     WhatType(v1)
     WhatType(v2)
     WhatType(v3)
     WhatType(v4)
+    WhatType(v5)
 }
+/*
 
+Вообще исходя из такси нам намекают на использование switch v.(type)
+
+но здесь как будто бы вообще можно воспользоваться 
+rv := reflect.Valueof(v)
+fmt.Println(rv.Kind())
+
+*/
